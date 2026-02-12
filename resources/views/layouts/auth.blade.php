184968f6fd35fffbb6f5e35db88554f20e81220e
kevin1234456 <ishkevin30@gmail.com>
2026-02-12 12:52:22 +0200

    Fix Tailwind CSS configuration and update gitignore to prevent view cache commits

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title') | E-LEARN</title>
     
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-black min-h-screen flex items-center justify-center p-6 text-white">
 
